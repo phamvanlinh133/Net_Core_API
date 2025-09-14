20537865834360236f6819a3d6af29969ca027e9
“phamvanlinh133” <phamvanlinh133@gmail.com>
2025-09-14 19:10:40 +0700

    ket-noi-va-lam-viec-voi-CSDL-11/9

--- PTPMQL25-26/DemoMVC/Models/Person.cs
+++ PTPMQL25-26/DemoMVC/Models/Person.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DemoMVC.Models
 {
+    [Table("Persons")]
     public class Person
     {
-        public int PersonId { get; set; }
+        [Key]
+        public string PersonId { get; set; }
         public string FullName { get; set; }
         public string Address { get; set; }
-        
+
     }
 }
